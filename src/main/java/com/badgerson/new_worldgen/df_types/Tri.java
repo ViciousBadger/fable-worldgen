3ecc489ce8f32e9da2b809bbe3531758342b8d5e
badgerson <mail@badgerson.com>
2025-06-17 21:37:32 +0200

    f off

--- src/main/java/com/badgerson/new_worldgen/df_types/Tri.java
+++ src/main/java/com/badgerson/new_worldgen/df_types/Tri.java
@@ -1,15 +1,16 @@
-package com.badgerson.density_function_types;
+package com.badgerson.new_worldgen.df_types;
 
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.util.KeyDispatchDataCodec;
-import net.minecraft.world.level.levelgen.DensityFunction;
-import net.minecraft.world.level.levelgen.DensityFunctions;
+import net.minecraft.util.dynamic.CodecHolder;
+import net.minecraft.world.gen.densityfunction.DensityFunction;
+import net.minecraft.world.gen.densityfunction.DensityFunctionTypes;
 
-public record Tri(DensityFunction df) implements DensityFunctions.PureTransformer {
 
-    private static final MapCodec<Sine> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(DensityFunction.HOLDER_HELPER_CODEC.fieldOf("argument").forGetter(Sine::df)).apply(instance, (Sine::new)));
-    public static final KeyDispatchDataCodec<Sine> CODEC  = DensityFunctions.makeCodec(MAP_CODEC);
+public record Tri(DensityFunction df) implements DensityFunctionTypes.Unary {
+
+    private static final MapCodec<Tri> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(DensityFunction.FUNCTION_CODEC.fieldOf("argument").forGetter(Tri::df)).apply(instance, (Tri::new)));
+    public static final CodecHolder<Tri> CODEC  = DensityFunctionTypes.holderOf(MAP_CODEC);
 
     @Override
     public DensityFunction input() {
@@ -17,14 +18,13 @@ public record Tri(DensityFunction df) implements DensityFunctions.PureTransforme
     }
 
     @Override
-    public double transform(double density) {
-    
-        // return Math.sin(density);
+    public double apply(double density) {
+        return 4.0 * Math.abs(density - Math.floor(density + 3.0 / 4.0) + 1.0 / 4.0) - 1.0;
     }
 
     @Override
-    public DensityFunction mapAll(Visitor visitor) {
-        return new Sine(this.df.mapAll(visitor));
+    public DensityFunction apply(DensityFunctionVisitor visitor) {
+        return new Tri(this.df.apply(visitor));
     }
 
     @Override
@@ -38,7 +38,7 @@ public record Tri(DensityFunction df) implements DensityFunctions.PureTransforme
     }
 
     @Override
-    public KeyDispatchDataCodec<? extends DensityFunction> codec() {
+    public CodecHolder<? extends DensityFunction> getCodecHolder() {
         return CODEC;
     }
 }
