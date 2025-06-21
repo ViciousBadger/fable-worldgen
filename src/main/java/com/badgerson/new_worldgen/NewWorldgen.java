3b23ce85ce47937e36e4a9ffd3dcaaa47f41e270
badgerson <mail@badgerson.com>
2025-06-21 22:06:57 +0200

    wee

--- src/main/java/com/badgerson/new_worldgen/NewWorldgen.java
+++ src/main/java/com/badgerson/new_worldgen/NewWorldgen.java
@@ -1,24 +1,32 @@
 package com.badgerson.new_worldgen;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.badgerson.new_worldgen.df_types.*;
+
 public class NewWorldgen implements ModInitializer {
 	public static final String MOD_ID = "new-worldgen";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+        Registry.register(
+            Registries.DENSITY_FUNCTION_TYPE,
+            Identifier.of("extra", "x"), XCoord.CODEC.codec()
+        );
+        Registry.register(
+            Registries.DENSITY_FUNCTION_TYPE,
+            Identifier.of("extra", "z"), ZCoord.CODEC.codec()
+        );
+        Registry.register(
+            Registries.DENSITY_FUNCTION_TYPE,
+            Identifier.of("extra", "tri"), Tri.CODEC.codec()
+        );
 	}
 }
---
+++ src/main/java/com/badgerson/new_worldgen/df_types/ZCoord.java
@@ -0,0 +1,30 @@
+package com.badgerson.new_worldgen.df_types;
+
+import com.mojang.serialization.MapCodec;
+import net.minecraft.util.dynamic.CodecHolder;
+import net.minecraft.world.gen.densityfunction.DensityFunction;
+
+public record ZCoord() implements DensityFunction.Base {
+
+    public static final CodecHolder<ZCoord> CODEC = CodecHolder.of(MapCodec.unit(new ZCoord()));
+
+    @Override
+    public double sample(DensityFunction.NoisePos pos) {
+        return Math.min(Math.max(pos.blockZ(),minValue()), maxValue());
+    }
+
+    @Override
+    public double minValue() {
+        return -30_000_000;
+    }
+
+    @Override
+    public double maxValue() {
+        return 30_000_000;
+    }
+
+    public CodecHolder<? extends DensityFunction> getCodecHolder() {
+        return CODEC;
+    }
+}
+
